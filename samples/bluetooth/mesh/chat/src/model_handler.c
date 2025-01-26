c3bb409d618c5e90bfa4bb29d4a164638eaf9d5c
rayshih <rayshih@sysage.com.tw>
2025-01-26 11:49:35 +0800

    加入 gen led on off srv
    加入 gen led on off srv

--- samples/bluetooth/mesh/chat/src/model_handler.c
+++ samples/bluetooth/mesh/chat/src/model_handler.c
@@ -30,14 +30,136 @@ static const struct shell *chat_shell;
 static struct k_work_delayable attention_blink_work;
 static bool attention;
 
+static void led_set(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    const struct bt_mesh_onoff_set *set,
+		    struct bt_mesh_onoff_status *rsp);
+
+static void led_get(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    struct bt_mesh_onoff_status *rsp);
+
+static const struct bt_mesh_onoff_srv_handlers onoff_handlers = {
+	.set = led_set,
+	.get = led_get,
+};
+
+struct led_ctx {
+	struct bt_mesh_onoff_srv srv;
+	struct k_work_delayable work;
+	uint32_t remaining;
+	bool value;
+};
+
+static struct led_ctx led_ctx[] = {
+#if DT_NODE_EXISTS(DT_ALIAS(led0))
+	{ .srv = BT_MESH_ONOFF_SRV_INIT(&onoff_handlers) },
+#endif
+#if DT_NODE_EXISTS(DT_ALIAS(led1))
+	{ .srv = BT_MESH_ONOFF_SRV_INIT(&onoff_handlers) },
+#endif
+#if DT_NODE_EXISTS(DT_ALIAS(led2))
+	{ .srv = BT_MESH_ONOFF_SRV_INIT(&onoff_handlers) },
+#endif
+#if DT_NODE_EXISTS(DT_ALIAS(led3))
+	{ .srv = BT_MESH_ONOFF_SRV_INIT(&onoff_handlers) },
+#endif
+};
+
+static void led_transition_start(struct led_ctx *led)
+{
+	int led_idx = led - &led_ctx[0];
+
+	/* As long as the transition is in progress, the onoff
+	 * state is "on":
+	 */
+	dk_set_led(led_idx, true);
+	k_work_reschedule(&led->work, K_MSEC(led->remaining));
+	led->remaining = 0;
+}
+
+static void led_status(struct led_ctx *led, struct bt_mesh_onoff_status *status)
+{
+	/* Do not include delay in the remaining time. */
+	status->remaining_time = led->remaining ? led->remaining :
+		k_ticks_to_ms_ceil32(k_work_delayable_remaining_get(&led->work));
+	status->target_on_off = led->value;
+	/* As long as the transition is in progress, the onoff state is "on": */
+	status->present_on_off = led->value || status->remaining_time;
+}
+
+static void led_set(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    const struct bt_mesh_onoff_set *set,
+		    struct bt_mesh_onoff_status *rsp)
+{
+	struct led_ctx *led = CONTAINER_OF(srv, struct led_ctx, srv);
+	int led_idx = led - &led_ctx[0];
+
+	if (set->on_off == led->value) {
+		goto respond;
+	}
+
+	led->value = set->on_off;
+	if (!bt_mesh_model_transition_time(set->transition)) {
+		led->remaining = 0;
+		dk_set_led(led_idx, set->on_off);
+		goto respond;
+	}
+
+	led->remaining = set->transition->time;
+
+	if (set->transition->delay) {
+		k_work_reschedule(&led->work, K_MSEC(set->transition->delay));
+	} else {
+		led_transition_start(led);
+	}
+
+respond:
+	if (rsp) {
+		led_status(led, rsp);
+	}
+}
+
+static void led_get(struct bt_mesh_onoff_srv *srv, struct bt_mesh_msg_ctx *ctx,
+		    struct bt_mesh_onoff_status *rsp)
+{
+	struct led_ctx *led = CONTAINER_OF(srv, struct led_ctx, srv);
+
+	led_status(led, rsp);
+}
+
+static void led_work(struct k_work *work)
+{
+	struct led_ctx *led = CONTAINER_OF(work, struct led_ctx, work.work);
+	int led_idx = led - &led_ctx[0];
+
+	if (led->remaining) {
+		led_transition_start(led);
+	} else {
+		dk_set_led(led_idx, led->value);
+
+		/* Publish the new value at the end of the transition */
+		struct bt_mesh_onoff_status status;
+
+		led_status(led, &status);
+		bt_mesh_onoff_srv_pub(&led->srv, NULL, &status);
+	}
+}
+
 static void attention_blink(struct k_work *work)
 {
 	static int idx;
 	const uint8_t pattern[] = {
-		BIT(0) | BIT(1),
-		BIT(1) | BIT(2),
-		BIT(2) | BIT(3),
-		BIT(3) | BIT(0),
+#if DT_NODE_EXISTS(DT_ALIAS(led0))
+		BIT(0),
+#endif
+#if DT_NODE_EXISTS(DT_ALIAS(led1))
+		BIT(1),
+#endif
+#if DT_NODE_EXISTS(DT_ALIAS(led2))
+		BIT(2),
+#endif
+#if DT_NODE_EXISTS(DT_ALIAS(led3))
+		BIT(3),
+#endif
 	};
 
 	if (attention) {
@@ -230,6 +352,26 @@ static struct bt_mesh_elem elements[] = {
 			BT_MESH_MODEL_CFG_SRV,
 			BT_MESH_MODEL_HEALTH_SRV(&health_srv, &health_pub)),
 		BT_MESH_MODEL_LIST(BT_MESH_MODEL_CHAT_CLI(&chat))),
+#if DT_NODE_EXISTS(DT_ALIAS(led0))
+	BT_MESH_ELEM(
+		2, BT_MESH_MODEL_LIST(BT_MESH_MODEL_ONOFF_SRV(&led_ctx[0].srv)),
+		BT_MESH_MODEL_NONE),
+#endif
+#if DT_NODE_EXISTS(DT_ALIAS(led1))
+	BT_MESH_ELEM(
+		3, BT_MESH_MODEL_LIST(BT_MESH_MODEL_ONOFF_SRV(&led_ctx[1].srv)),
+		BT_MESH_MODEL_NONE),
+#endif
+#if DT_NODE_EXISTS(DT_ALIAS(led2))
+	BT_MESH_ELEM(
+		4, BT_MESH_MODEL_LIST(BT_MESH_MODEL_ONOFF_SRV(&led_ctx[2].srv)),
+		BT_MESH_MODEL_NONE),
+#endif
+#if DT_NODE_EXISTS(DT_ALIAS(led3))
+	BT_MESH_ELEM(
+		5, BT_MESH_MODEL_LIST(BT_MESH_MODEL_ONOFF_SRV(&led_ctx[3].srv)),
+		BT_MESH_MODEL_NONE),
+#endif
 };
 /* .. include_endpoint_model_handler_rst_1 */
 
@@ -423,6 +565,10 @@ SHELL_CMD_ARG_REGISTER(chat, &chat_cmds, "Bluetooth Mesh Chat Client commands",
 const struct bt_mesh_comp *model_handler_init(void)
 {
 	k_work_init_delayable(&attention_blink_work, attention_blink);
+
+	for (int i = 0; i < ARRAY_SIZE(led_ctx); ++i) {
+		k_work_init_delayable(&led_ctx[i].work, led_work);
+	}
 
 	chat_shell = shell_backend_uart_get_ptr();
 	shell_print(chat_shell, ">>> Bluetooth Mesh Chat sample <<<");
